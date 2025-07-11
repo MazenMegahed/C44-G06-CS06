b843189b6219b5bdc875c3a230b29747268eb87c
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-11 22:45:14 +0300

    Question 2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Session6_Assignment
+﻿using System.ComponentModel;
+
+namespace Session6_Assignment
 {
     internal class Program
     {
@@ -8,15 +10,38 @@
             //Explain the difference between passing (Value type parameters) by value and by reference then write a suitable c# example.
             //passing by value : any change happens in the function doesn't reflect on the original variable in main code 
             // passing by reference : any change happens in the function reflects on the original variable in main code 
-            int x = 5; int y = 6;
-            Console.WriteLine($"before : X = {x} , X = {y}");
-            increaseby1(ref x, ref y);
-            Console.WriteLine($"after : X = {x} , X = {y}");
+            //int x = 5; int y = 6;
+            //Console.WriteLine($"before : X = {x} , X = {y}");
+            //increaseby1(ref x, ref y);
+            //Console.WriteLine($"after : X = {x} , X = {y}");
+            #endregion
+
+            #region Q2
+            // Explain the difference between passing (Reference type parameters) by value and by reference then write a suitable c example.
+            //passing by value : change the object's content but don't affect the reference in heap
+            // passing by reference : change the object's content and create new reference in heap
+            int[] Array = { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Main hashcode : "+Array.GetHashCode());
+            changelastelementbyvalue(Array);
+            Console.WriteLine("Hashcode after change last by value : " + Array.GetHashCode());
+            changelastelementbyreference(ref Array);
+            Console.WriteLine("Hashcode after change last by reference : " + Array.GetHashCode());
+
             #endregion
         }
         public static void increaseby1(ref int a, ref int b)
         {
             a += 1; b += 1;
+        }
+
+        public static void changelastelementbyvalue(int[]objects)
+        {
+            objects[objects.Length-1] += 5; 
+        }
+        static void changelastelementbyreference(ref int[] objects)
+        {
+            // This actually replaces the array with a new one
+            objects = new int[] { 9, 9, 9, 9, 9 };
         }
 
     }
