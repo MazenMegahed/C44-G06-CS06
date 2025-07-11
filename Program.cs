6e69f6ee7748c1959519c132da1449cf284a0fdc
Mazen Megahed <mazen.megahed1@outlook.com>
2025-07-11 22:18:23 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session6_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
